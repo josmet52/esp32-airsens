676ac23f84d10d1195f824053c1d75d849209d74
josmet52 <jom52.dev@gmail.com>
2026-06-04 12:31:32 +0200

    essai mise à jour github

--- airsens_v3.py
+++ airsens_v3.py
@@ -12,7 +12,7 @@ data management for the project airsens esp32-mqtt-mysql
 v0.1.0 : 19.08.2022 --> first prototype based on airsens_mqtt.py
 v2.0.0 : 22.04.2023 --> adapté pour airsens_v2
 v3.0.0 : 22.11.2023 --> adapté pour airsens_v3
-vx.y.z : 07.06.2026 --> test github
+vx.y.z : 07.06.2026 --> test github final
 """
 VERSION = '3.0.0'
 APP = 'airsens_v3.py'
